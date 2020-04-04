af40cc6b6a15cbbe61c6bf4dd14c8570ae177014
alperenkbd <alperen_kabadayi@hotmail.com>
2020-04-04 20:40:01 +0300

    google analytics

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = ({ posts }) => (
   <div className="container">
         <Head>
 
-            <!-- Global site tag (gtag.js) - Google Analytics -->
+            
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162642938-1"></script>
             <script>
                 window.dataLayer = window.dataLayer || [];
