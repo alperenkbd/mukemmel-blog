ed4f8514132f3bfad59ac349bb4fecf353952cff
alperenkbd <alperen_kabadayi@hotmail.com>
2020-04-04 21:03:13 +0300

    follow code

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,15 @@ const Home = ({ posts }) => (
 
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162642938-1"></script>
 
-            <!-- Global site tag (gtag.js) - Google Analytics -->
+            
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162642938-1"></script>
-            <script>
+                    <script>
                 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+                 function gtag(){dataLayer.push(arguments)  ;  }
                 gtag('js', new Date());
               
                 gtag('config', 'UA-162642938-1');
-</script>
+        </script>
 
 
         </Head>
